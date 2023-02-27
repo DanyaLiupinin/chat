9181ffd33f8efb1f80655f1d8e2ea19bd612876d
Danya Kovan <mr.kovan_2002@mail.ru>
2023-02-27 18:24:02 +0400

    submit disabled when !loggedIn

--- src/components/ChatInput/ChatInput.js
+++ src/components/ChatInput/ChatInput.js
@@ -19,10 +19,9 @@ function ChatInput({ onSendMessage, loggedIn, currentUser }) {
         <div className='chatInput'>
             <form className={`chatInput__form ${!loggedIn ? 'inactive' : ''}`} onSubmit={handleSubmit} disabled={!loggedIn || message === '' ? true : false}>
                 <textarea className='chatInput__input' type='text' name='message' readOnly={!loggedIn ? true : false} placeholder='введите сообщение' onChange={(e) => { setMessage(e.target.value) }} value={message || ''}></textarea>
-                <button className={`chatInput__submit-button ${!message === '' ? 'chatInput__submit-button_inactive' : ''}`} type='submit'>
+                <button className={`chatInput__submit-button ${!message === '' ? 'chatInput__submit-button_inactive' : ''}`} type='submit' disabled={!loggedIn ? true : false}>
                     <img className='chatInput__submit-image' alt='send message' src={sendMessage}></img>
                 </button>
-                
             </form>
             <p className='chatInput__caption'>для отправки нажмите tab+enter</p>
         </div>

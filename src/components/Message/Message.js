fa340a264dbeef7a76f063a8538f4f1ab7518937
Danya Kovan <mr.kovan_2002@mail.ru>
2023-03-08 12:08:55 +0400

    feat: username added to message

--- src/components/Message/Message.js
+++ src/components/Message/Message.js
@@ -1,11 +1,21 @@
 import './Message.css'
 
-function Message (props) {
+function Message(props) {
 
     return (
         <div className='message'>
-            <div className={`message__text-container ${props.messageUser === props.currentUser.username ? 'message__text-container_type_mine' : 'message__text-container_type_notmine'}`}>
-                <p className='message__text'>{props.text}</p>
+            <div className={`message__container ${props.messageUser === props.currentUser.username ? 'message__container_type_mine' : 'message__container_type_notmine'}`}>
+
+                <div className={`message__name-container ${props.messageUser === props.currentUser.username ?
+                    'message__name-container_type_mine' : 'message__name-container_type_notmine'}`}>
+                    <p className='message__name message__name_type_mine'>{props.messageUser}</p>
+                </div>
+
+                <div className={`message__text-container ${props.messageUser === props.currentUser.username ? 'message__text-container_type_mine' : 'message__text-container_type_notmine'}`}>
+                    <p className='message__text'>{props.text}</p>
+                </div>
+
+
             </div>
         </div>
     )

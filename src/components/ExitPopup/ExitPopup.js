4abf022682b5b1cd5e1179e11b0136d23269697d
Danya Kovan <mr.kovan_2002@mail.ru>
2023-03-09 14:14:40 +0400

    feat: buttons design for ExitPopup added

--- src/components/ExitPopup/ExitPopup.js
+++ src/components/ExitPopup/ExitPopup.js
@@ -1,8 +1,15 @@
 import './ExitPopup.css'
 
+import krestik from '../../images/krestik.png'
+
 const ExitPopup = () => {
     return (
         <div className='exitPopup'>
+            
+            <button className='exitPopup__button exitPopup__button_type_сlose' type='button'>
+                <img className='exitPopup__button-image exitPopup__button-image_type_delete' alt='закрыть попап' src={krestik} ></img>
+            </button>
+
             <button className='exitPopup__button exitPopup__button_type_clear' type='button'>Очистить чат</button>
             <button className='exitPopup__button exitPopup__button_type_signout' type='button'>Выйти</button>
         </div>
